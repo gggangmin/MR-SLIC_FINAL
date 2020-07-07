752954f942712b8edb15735c8c7ab2562c2ee722
Ubuntu <ubuntu@ip-172-31-25-150.us-west-2.compute.internal>
2020-07-07 12:48:05 +0000

    pyx

--- setup/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(name='slic app',
-      ext_modules=cythonize("_slic_master.pyx"),
-      include_dirs=[numpy.get_include()])
